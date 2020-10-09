52578b19d34790da1db3b9f0b2af67b5da0cee98
ezequiel3434 <ezequiel3434@gmail.com>
2020-10-09 17:45:42 -0300

    adding 3rd libraries:  Alamofire and swiftyJson

--- ClimaApp/Model/WeatherManager.swift
+++ ClimaApp/Model/WeatherManager.swift
@@ -10,6 +10,10 @@ import UIKit
 import CoreLocation
 
 class WeatherManager: NSObject  {
+    
+    let weatherURL = "api.openweathermap.org/data/2.5/weather?appid=a1d0288ce3b03e2bb18e89db9c5ced8c&units=metric"
+    
+    
     var currentLocation: CLLocation?
     
     let locationManager = CLLocationManager()
@@ -29,6 +33,32 @@ class WeatherManager: NSObject  {
         let long = currentLocation.coordinate.longitude
         let lat = currentLocation.coordinate.latitude
         print("\(long) | \(lat)")
+    }
+    
+    
+    func fetchWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        let urlString = "\(weatherURL)&lat=\(latitude)&lon=\(longitude)"
+        
+    }
+    
+    func performRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url) { (data, respinse, error) in
+                if error != nil {
+                    return
+                }
+                
+                if let safeData = data {
+                    
+                }
+            }
+            
+            
+            
+        }
     }
     
    
