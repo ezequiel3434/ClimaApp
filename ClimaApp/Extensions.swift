20d742c76d37c5d2f15594537a9aade0a73f3624
ezequiel3434 <ezequiel3434@gmail.com>
2020-10-11 16:11:01 -0300

    feature under construction(Weather Table View Cell) and adding a Date extension, day OfTheWeek

--- ClimaApp/Extensions.swift
+++ ClimaApp/Extensions.swift
@@ -25,5 +25,11 @@ extension Date {
         return dateFormatter.string(from: self)
     }
     
+    func dayOfTheWeek() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        
+        return dateFormatter.string(from: self)
+    }
     
 }
