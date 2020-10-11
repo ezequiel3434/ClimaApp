2257e7926521615b845305df06b31096a28ad5f9
ezequiel3434 <ezequiel3434@gmail.com>
2020-10-11 15:51:57 -0300

    features under construction (Info Collection View Cell , WeatherInfo model)

---
+++ ClimaApp/View/InfoCVCell/InfoCollectionViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  InfoCollectionViewCell.swift
+//  ClimaApp
+//
+//  Created by Ezequiel Parada Beltran on 11/10/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import UIKit
+
+class InfoCollectionViewCell: UICollectionViewCell {
+
+    //MARK: - IBOutlets
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var infoImageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func generateCell(weatherInfo: WeatherInfo) {
+        infoLabel.text = weatherInfo.infoText
+        infoLabel.adjustsFontSizeToFitWidth = true
+        if weatherInfo.image != nil {
+            nameLabel.isHidden = true
+            infoImageView.isHidden = false
+            infoImageView.image = weatherInfo.image
+        } else {
+            // no image
+            nameLabel.isHidden = false
+            infoImageView.isHidden = true
+            nameLabel.adjustsFontSizeToFitWidth = true
+            nameLabel.text = weatherInfo.nameText
+        }
+    }
+}
---
+++ ClimaApp/WeatherInfo.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherInfo.swift
+//  ClimaApp
+//
+//  Created by Ezequiel Parada Beltran on 11/10/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct WeatherInfo {
+    var infoText: String!
+    var nameText: String?
+    var image: UIImage?
+}
