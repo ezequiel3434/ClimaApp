30e8f1008bcf62b1ba9cb52e1502b9acbe4dac0b
ezequiel3434 <ezequiel3434@gmail.com>
2020-10-12 23:18:25 -0300

    feature under construction(parsing CSV and Choose city table view functions)

--- ClimaApp/Controller/ChooseCityViewController.swift
+++ ClimaApp/Controller/ChooseCityViewController.swift
@@ -14,21 +14,69 @@ class ChooseCityViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
+    //MARK: - Vars
+    var allLocations: [WeatherLocation] = []
+    var filteredLocations: [WeatherLocation] = []
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        loadLocationsFromCSV()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - Get Locations
+    
+    private func loadLocationsFromCSV() {
+        if let path = Bundle.main.path(forResource: "location", ofType: "csv") {
+            parseCSVAt(url: URL(fileURLWithPath: path))
+        }
     }
-    */
+    
+    private func parseCSVAt(url: URL){
+        do {
+            let data = try Data(contentsOf: url)
+            let dataEncoded = String(data: data, encoding: .utf8)
+            if let dataArr = dataEncoded?.components(separatedBy: "\n").map({$0.components(separatedBy: ",")}) {
+                var i = 0
+                for line in dataArr {
+                    if line.count > 2 && i != 0 {
+                        createLocation(line: line)
+                    }
+                    
+                    i += 1
+                }
+            }
+        } catch {
+            print("Error reading CSV file, ", error.localizedDescription)
+        }
+    }
+    
+    private func createLocation(line: [String]){
+        
+        allLocations.append(WeatherLocation(city: line.first!, country: line.last!, countryCode: line[1], isCurrentLocation: false))
+        
+    }
 
+}
+
+extension ChooseCityViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredLocations.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath )
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // save location
+    }
+    
+    
 }
