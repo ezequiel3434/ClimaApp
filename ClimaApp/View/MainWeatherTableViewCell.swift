3981be1ebc366682824845076416c294c020b540
ezequiel3434 <ezequiel3434@gmail.com>
2020-10-13 04:17:21 -0300

    MainWeather Custom Cell in AllLocationsTVC

---
+++ ClimaApp/View/MainWeatherTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MainWeatherTableViewCell.swift
+//  ClimaApp
+//
+//  Created by Ezequiel Parada Beltran on 13/10/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import UIKit
+
+class MainWeatherTableViewCell: UITableViewCell {
+    //MARK: - IBOutlets
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+    
+}
